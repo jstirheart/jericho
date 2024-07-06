8d6b2d666bcd0eba0e710aa6555f8687c484591a
J♥ <CtrlAltJ@protonmail.com>
2024-07-05 22:29:57 -0500

    Implementation of Additional File Acquisition list, Custom filename regex, Disableflag for Tracert

--- jericho.py
+++ jericho.py
@@ -19,6 +19,7 @@ from shadowcopy import shadow_copy as scopy
 
 videoType = re.compile('.*[.](?:MP4|H264|MOV|AVI|WMV|FLV|F4V|MKV|WebM|AVCHD|MPEG2|3GP|3G2|OGV|M4V|ProRes)$', re.IGNORECASE)
 otherType = re.compile('.*[.](?:M4A)$', re.IGNORECASE)#TODO: currently configured for debug/testing, set to img types
+cliType = None
 hashfeed=["b9ab94fd6a2e6b3f0d841529851f8db8","ba51c40c5eb8fdc2f9a98a28470b09fb"]#TODO: replace sample innocuous MD5 with target hash collection of value (sample is an AMD, ATI dll file and of a sample file found hash on test machine)
 #Design implementation guide:
 #Load hashfeed through external feed file such that compilation of hashes is negotiated by secure server share and encryption key
@@ -34,6 +35,7 @@ reportIP=""
 reportHostname=""
 reportCSVPath=["filepath,md5"]
 reportLogTime=["datetime,message,detail"]
+acquireList=[]
 
 def debug(statement=""):
     f = inspect.currentframe().f_back
@@ -58,9 +60,15 @@ def acquire(outpath=os.getcwd()+'\\jeResults\\acq'):#define: Acquisition of high
         if(os.path.isdir(tPath) and os.path.exists(src)):
            getfile(src,dstDir) #Acquire existing user ntuser.dat hive
 
-    reghives=["C:\Windows\System32\config\SAM","C:\Windows\System32\config\SECURITY","C:\Windows\System32\config\SOFTWARE","C:\Windows\System32\config\SYSYTEM"]
+    reghives=["C:\Windows\System32\config\SAM","C:\Windows\System32\config\SECURITY","C:\Windows\System32\config\SOFTWARE","C:\Windows\System32\config\SYSTEM"]
     for x in reghives:
         getfile(x,outpath) #Acquire critical windows registry hives
+
+    for x in acquireList:
+        try:
+            getfile(x,outpath)
+        except Exception as e:
+            handleExcept(e)
     None
 
 def getfile(src, dstDir):#define: Perform a attempt to metadata preserving copy of file @src to @dstDir, Failing by permissions, attempt via volume shadow copy technique against locked files. 
@@ -73,6 +81,8 @@ def getfile(src, dstDir):#define: Perform a attempt to metadata preserving copy 
             shutil.copy2(src,dstDir) #Acquire high value log containing usb device connection artifacts
             if os.path.exists(dst):
                     appendLogTime(message="Acquisition: %s"%(src),detail=md5(dst))
+        else:
+            appendLogTime(message="ACQ Skipped(File DNE): %s"%src)
     except PermissionError:
         try:
             scopy(src,dstDir) #Acquire high value log containing usb device connection artifacts
@@ -97,7 +107,7 @@ def alert(hash):#define: Triggered match from hashfeed, initiate artifact collec
     debug("HI from ^litte one")
     appendLogTime("Alert Raised on value",hash)
     if (not reportFlagAlert):
-        if(not args.d):
+        if(not args.cliFlagTracerMakeADifference):
             reportTrace=gettrace()#build ancillory report on visibility to known common target
         reportIP=getip()#get public facing ip of device
         reportHostname=subprocess.check_output(["hostname"])#get the device hostname
@@ -131,19 +141,29 @@ def writeReport(outpath=os.getcwd()+'\\jeResults'):
 
 def main():
     global args
+    global cliType
     parser = argparse.ArgumentParser(description='Forensics response script to triage [VILE] hash match')
     parser.add_argument("--path", help="Start path to begin recursive descent from. Unspecified means walk each of device's Mounts",type=str)
     parser.add_argument("--loadHashFileP", help="Designates path of unencrypted list of [VILE] hashes. Format 1 md5 / line",type=str)
-    parser.add_argument("-d",help="DEBUG FLAG", action='store_true')
+    parser.add_argument("--re", help="Custom regex to match filename to, EX: '.*[.](?:MP4|H264|MOV|AVI|WMV|FLV|F4V|MKV|WebM|AVCHD|MPEG2|3GP|3G2|OGV|M4V|ProRes|M4A)$'",type=str)
+    parser.add_argument("-d",help="Disable tracert, greatly affects speed on tests/regex development", action='store_true', dest='cliFlagTracerMakeADifference')
+    parser.add_argument("--acquireFiles", help="Path to .txt containing listing of files to acquire if TRIGGERED. Format 1 file path / line",type=str)
     args = parser.parse_args()
     appendLogTime("Application began execution")
     #debug
-    #args.path="E:\\Code\\"
+    args.path="E:\\Code\\"
     #args.loadHashFileP="E:\\Code\\jericho\\test.hash"
+    #args.re=".*"
+    args.acquireFiles="E:\\Code\\jericho\\jericho\\testAcqPath.txt"
     #
     try:
         if args.loadHashFileP:
             loadHashFeedUNENC(args.loadHashFileP)
+        if args.re:
+            cliType=re.compile(args.re, re.IGNORECASE)
+        if args.acquireFiles:
+            loadAcquireList(args.acquireFiles)
+
 
         if (not args.path):#No path specified, step through all mounted partitions
             partition = psutil.disk_partitions()
@@ -167,6 +187,13 @@ def loadHashFeedUNENC(path):#define: This function loads an unencrypted hash fee
             hashfeed.append(line.strip('\r').strip('\n'))
     None
 
+def loadAcquireList(path):#define: This function loads an filelist of additionally requested files to be acquired given an alert match. FORMAT str, one per line ('\r\n' delimited), MAY SPECIFY DYNAMIC USER FOLDERS USING "?USER" ex: "C:\Users\?USER\NTUSER"
+    acquireList.clear()
+    with open(path) as inf:
+        for line in inf:
+            acquireList.append(line.strip('\r').strip('\n'))
+    None
+
 def handleExcept(e,postmessage=""):
     appendLogTime("INFO-WARN: %s %s"%(str(e),postmessage))
     print('\x1b[6;30;42m' + str(e)+"\n^^^^^^^^^^^^^^^^^^"+ '\x1b[0m')
@@ -184,7 +211,7 @@ def examine(path):#define: given a filepath, recursively explore all directories
             if(os.path.isdir(x)):
                 examine(x+r'\\')
 
-            if(videoType.match(x) or otherType.match(x)):
+            if(videoType.match(x) or otherType.match(x) or (cliType and cliType.match(x))):
                 print('==========================================HASH REQUEST=============================================================')
                 hash=md5(x)
                 reportCSVPath.append(x+','+hash)#passively collect report of videohashes along way in case of triggering alert csvfmt<FILEPATH,MD5>
@@ -219,6 +246,7 @@ def getip(iface=None): #define: issue command {curl ifcfg.me} to identify public
     None
     
 def gettrace(target="8.8.8.8"): #define: issue command {tracert 8.8.8.8} to identify network characteristics to known destination @target=specify ip connection string
+    print("Enacting TRACERT- caution: patience please, get you a cup of coffee, you deserve it <3")
     return subprocess.check_output(["tracert",target])
     None
 
